31735b77c82f9a7d7133ca1e426d5b94bda8ba6c
skooog <susan.skoog@gmail.com>
2014-10-03 19:08:44 -0400

    changed database.yml to copy of onemonthrails one, so perf sqlite3

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get "about" => "pages#about" #creates about_path
 
   get "anotherpage" => "pages#anotherpage"
+
+  get "signup" => "pages#signup"
+
+  get "login" => "pages#login"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
